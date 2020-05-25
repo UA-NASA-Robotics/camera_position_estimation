e0a4c37146a8b1996e9400a0817af57ff08c5502
David Klett <david.ger.klett@gmail.com>
2020-05-25 14:04:57 -0400

    Change usb channel

--- can_test.py
+++ can_test.py
@@ -13,8 +13,10 @@ msg = can.Message(arbitration_id=0xc0ffee,
                   data=[0, 25, 0, 1, 3, 1, 4, 1],
                   is_extended_id=True)
 
-try:
-    bus.send(msg)
-    print("Message sent on {}".format(bus.channel_info))
-except can.CanError:
-    print("Message NOT sent")
+while(1):
+
+  try:
+      bus.send(msg)
+      print("Message sent on {}".format(bus.channel_info))
+  except can.CanError:
+      print("Message NOT sent")
